2311866af746b846809df4baf03a057babfedaf9
Sadam Hussain <d_shussain@universalweather.net>
2024-05-18 16:26:38 -0400

    Add Projects and Tech Stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
-import HeroSection from "@/components/modules/Hero";
+"use client";
 
-export default function Home() {
+import { navItems } from "@/data";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import Hero from "@/components/modules/Hero";
+import Grid from "@/components/ui/Grid";
+import RecentProjects from "@/components/modules/RecentProjects";
+
+const Home = () => {
   return (
-   <main>
-     <HeroSection />
-   </main>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        {/*<Clients />*/}
+        {/*<Experience />*/}
+        {/*<Approach />*/}
+        {/*<Footer />*/}
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
